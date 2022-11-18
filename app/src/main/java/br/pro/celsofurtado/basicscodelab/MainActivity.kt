013c14b6e53247abf810c94567680143d4444d1d
Celso Furtado <celsofurtado@terra.com.br>
2022-11-18 16:49:10 -0300

    Basic Codelab Finalizado - Jetpack Compose

--- app/src/main/java/br/pro/celsofurtado/basicscodelab/MainActivity.kt
+++ app/src/main/java/br/pro/celsofurtado/basicscodelab/MainActivity.kt
@@ -1,24 +1,57 @@
 package br.pro.celsofurtado.basicscodelab
 
+//import android.content.res.Configuration.UI_MODE_NIGHT_YES
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.animation.animateContentSize
+//import androidx.compose.animation.core.Spring
+//import androidx.compose.animation.core.spring
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.lazy.LazyColumn
+//import androidx.compose.foundation.lazy.items
+//import androidx.compose.material.*
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.ExpandLess
+//import androidx.compose.material.icons.filled.ExpandMore
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.*
+//import androidx.compose.runtime.saveable.rememberSaveable
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,14 +60,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.pro.celsofurtado.basicscodelab.ui.theme.BasicsCodelabTheme
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                MyApp()
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -62,33 +94,27 @@ fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = List(1000) { "$it" }
 ) {
-
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
             Greeting(name = name)
         }
     }
-
-//    Column(modifier = modifier.padding(vertical = 4.dp)) {
-//        for (name in names) {
-//            Greeting(name = name)
-//        }
-//    }
-
 }
 
 @Composable
-fun Greeting(name: String) {
+private fun Greeting(name: String){
+    Card(
+        backgroundColor = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name = name)
+    }
+}
 
-    val expanded = rememberSaveable { mutableStateOf(false) }
+@Composable
+fun CardContent(name: String) {
 
-    val extraExpanding by animateDpAsState(
-        if (expanded.value) 100.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -96,13 +122,18 @@ fun Greeting(name: String) {
     ) {
         Row(
             modifier = Modifier
-                .padding(24.dp)
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
             Column(
                 modifier = Modifier
-                    .weight(0.6f)
-                    .padding(bottom = extraExpanding.coerceAtLeast(0.dp))
-
+                    .weight(1f)
+                    .padding(12.dp)
             ) {
                 Text(
                     text = "Hello,",
@@ -112,24 +143,33 @@ fun Greeting(name: String) {
                 )
                 Text(
                     text = "$name!",
-                    style = MaterialTheme.typography.headlineMedium
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
 
                     //fontSize = 20.sp,
                     //fontWeight = FontWeight.Bold,
                     //color = Color.Yellow
                 )
+                if(expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
             }
-
-            //Text(text = "Teste", modifier = Modifier.weight(0.2f).background(Color.Blue))
-
-            ElevatedButton(onClick = {
-                expanded.value = !expanded.value
-            }, modifier = Modifier.weight(0.4f)) {
-                Text(
-                    if (expanded.value) "Show less" else "Show more"
+            IconButton(onClick = {
+                expanded = !expanded
+            }, modifier = Modifier.padding(top = 0.dp)) {
+                Icon(
+                    imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        "Expand Less"
+                    } else {
+                        "Expand More"
+                    }
                 )
             }
-
         }
     }
 }
